5431e31789f4af2d30f09d4110fb897934435c8d
altaf1232 <139534724+altaf1232@users.noreply.github.com>
2023-07-14 18:14:41 +0530

    Add files via upload

---
+++ TestCurdOperation/src/main/java/com/curdoperation/TestSetterAndGetterClass.java
@@ -0,0 +1,42 @@
+package com.curdoperation;
+
+public class TestSetterAndGetterClass 
+{
+   private  int Employee_id;
+   private String name;
+   private String address;
+   private String city;
+   private String email;
+public int getEmployee_id() {
+	return Employee_id;
+}
+public void setEmployee_id(int employee_id) {
+	Employee_id = employee_id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+   
+  
+}
